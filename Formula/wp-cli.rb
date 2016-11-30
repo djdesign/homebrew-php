c11e5aa855a2867d95788a1cf877a8e5121898b8
Daniel Willitzer <daniel@thedjdesign.com>
2016-11-30 00:02:10 -0800

    Update wp-cli.rb
    Version Bump to Latest Stable Release v1.0.0

--- Formula/wp-cli.rb
+++ Formula/wp-cli.rb
@@ -5,7 +5,7 @@ class WpCli < Formula
 
   desc "Command-line tools for managing WordPress installations."
   homepage "http://wp-cli.org/"
-  url "https://github.com/wp-cli/wp-cli/archive/v0.25.0.tar.gz"
+  url "https://github.com/wp-cli/wp-cli/archive/v1.0.0.tar.gz"
   sha256 "98526f880619bf95959ae9812204af7de6949e295dc1dcb4f9007d0355c4f7c7"
   head "https://github.com/wp-cli/wp-cli.git"
 
